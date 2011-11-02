b3218c5e3f04ffcefe53905c0c33d1f1b3a6759b
FabianoPS <fabianosoriani@gmail.com>
2011-11-02 02:08:24 -0200

    scratch version

---
+++ src/com/phonegamp/hello/App.java
@@ -0,0 +1,15 @@
+package com.phonegamp.hello;
+
+//import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
